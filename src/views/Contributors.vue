d5d0f984c784185a6e8d94128b20eda7d2148934
Allen Shaji <allenshaji10@gmail.com>
2020-10-03 10:55:23 +0530

    new

---
+++ src/views/Contributors.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Contributors</h1>
+    <p><a href="https://github.com/allenshaji">Allen Shaji</a></p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Hackoberfest logo" src="../assets/download.jpeg">
+    <HelloWorld msg="Welcome to Hacktoberfest 2020"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
